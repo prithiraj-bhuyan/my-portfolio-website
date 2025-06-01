2fbc32854025a3397f8c2882443a47ea6aa730c6
Prithiraj Bhuyan <66234297+prithiraj-bhuyan@users.noreply.github.com>
2025-06-01 19:18:13 +0530

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -27,7 +27,7 @@ const Footer = () => {
                 {
                     socialMedia.map((profile) => (
                         <div key={profile.id} className='w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300'>
-                            <img src={profile.img} alt="icons" width={20} height={20} />
+                            <img src={profile.img} href={profile.link} alt="icons" width={20} height={20} />
                         </div>
                     ))
                 }
